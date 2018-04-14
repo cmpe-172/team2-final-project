4a6a10385b415435651ffbfe2e7a6556b0c076ae
Tim Davis <timothy.davis@sjsu.edu>
2018-04-14 15:37:41 -0700

    added home page with login frontend
    -added the UI for a homepage with a navbar.
    -link to Login page UI.
    -from tutorial:
    https://serverless-stack.com/chapters/create-a-login-page.html

--- react-backend/client/src/App.js
+++ react-backend/client/src/App.js
@@ -1,36 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import "./App.css";
+
+import { LinkContainer } from "react-router-bootstrap";
+
+import Routes from "./Routes";
 
 class App extends Component {
-
-  state = {
-    users: []
-  }
-
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json()) // in order to get this express needs to running too. 
-      .then(users => this.setState({users})); // add user list to state object.
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-
-        <h1> Users: </h1>
-        <ul>
-          {this.state.users.map(user =>
-            <li key={user.id}> {user.username} </li>
-          )}
-        </ul>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App container">
+                <Navbar fluid collapseOnSelect>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <Link to="/">Scratch</Link>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                        <Nav pullRight>
+                            <LinkContainer to="/signup">
+                                <NavItem>Signup</NavItem>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <NavItem>Login</NavItem>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                <Routes />
+            </div>
+        );
+    }
 }
 
 export default App;
--- react-backend/client/src/index.js
+++ react-backend/client/src/index.js
@@ -4,5 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById("root")
+);
+
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
